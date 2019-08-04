8581079843de7d94ee689ff67b6310be37704c4d
virajtk <viraj.pop3@yahoo.com>
2019-08-04 14:13:40 +0530

    This is the Main Project of Uni Thalluwa Project by Team Scorpion First I implement the login screen Others do their own function on this and pull

---
+++ app/src/main/java/com/scorpion/unithalluwa/ui/login/LoginViewModelFactory.java
@@ -0,0 +1,26 @@
+package com.scorpion.unithalluwa.ui.login;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.annotation.NonNull;
+
+import com.scorpion.unithalluwa.data.LoginDataSource;
+import com.scorpion.unithalluwa.data.LoginRepository;
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+public class LoginViewModelFactory implements ViewModelProvider.Factory {
+
+    @NonNull
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(LoginViewModel.class)) {
+            return (T) new LoginViewModel(LoginRepository.getInstance(new LoginDataSource()));
+        } else {
+            throw new IllegalArgumentException("Unknown ViewModel class");
+        }
+    }
+}
