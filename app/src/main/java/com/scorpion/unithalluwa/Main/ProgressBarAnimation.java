b4179b80bb6d816144c843431faabb0ebc4ae491
virajtk <viraj.pop3@yahoo.com>
2019-09-10 17:51:14 +0530

    This is the Main Project of Uni Thalluwa Project by Team Scorpion First I implement the login screen Others do their own function on this and pull I have done the user profile, login and register - Viraj

---
+++ app/src/main/java/com/scorpion/unithalluwa/Main/ProgressBarAnimation.java
@@ -0,0 +1,39 @@
+package com.scorpion.unithalluwa.Main;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.scorpion.unithalluwa.ui.login.LoginActivity;
+
+public class ProgressBarAnimation extends Animation {
+
+    private Context context;
+    private ProgressBar progressBar;
+    private TextView textView;
+    private float from;
+    private float to;
+
+    public ProgressBarAnimation(Context context, ProgressBar progressBar, TextView textView, float from, float to) {
+        this.context = context;
+        this.progressBar = progressBar;
+        this.textView = textView;
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    protected void applyTransformation(float interpolatedTime, Transformation t) {
+        super.applyTransformation(interpolatedTime, t);
+        float value = from +  (to - from) * interpolatedTime;
+        progressBar.setProgress((int)value);
+        textView.setText((int)value + " %");
+
+        if (value == to) {
+            context.startActivity(new Intent(context, LoginActivity.class));
+        }
+    }
+}
