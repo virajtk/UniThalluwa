68e8f43bd7f3475e94b8d11fd6c9aaa7d36f4b4f
Hasindu98 <hasinduheshan1998@gmail.com>
2019-09-11 12:26:18 +0530

    my function is Assignment function.three main interfaces,Add assignment,Retrive assignment,edit Assignment.  Assignmnet - Hasiya

--- app/src/main/java/com/scorpion/unithalluwa/Assingment/AddAssignment.java
+++ app/src/main/java/com/scorpion/unithalluwa/Assingment/AddAssignment.java
@@ -3,6 +3,10 @@ package com.scorpion.unithalluwa.Assingment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.scorpion.unithalluwa.R;
 
@@ -12,5 +16,8 @@ public class AddAssignment extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_assignment);
+
+
+
     }
 }
--- app/src/main/java/com/scorpion/unithalluwa/Assingment/RetriveAssignment.java
+++ app/src/main/java/com/scorpion/unithalluwa/Assingment/RetriveAssignment.java
@@ -3,6 +3,8 @@ package com.scorpion.unithalluwa.Assingment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.scorpion.unithalluwa.R;
 
@@ -12,5 +14,12 @@ public class RetriveAssignment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_retrive_assignment);
+
+        Spinner  mySpinner = (Spinner) findViewById(R.id.spinner);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(RetriveAssignment.this,
+                android.R.layout.activity_list_item,getResources().getStringArray(R.array.DropDownBox));
+            myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            mySpinner.setAdapter(myAdapter);
     }
 }
