459a5b929634334331f3065eaeef66e115654662
Hasindu98 <hasinduheshan1998@gmail.com>
2019-09-22 19:19:18 +0530

    UI ASsignment -  Hasindu

--- app/src/main/java/com/scorpion/unithalluwa/Assingment/EditAssignment.java
+++ app/src/main/java/com/scorpion/unithalluwa/Assingment/EditAssignment.java
@@ -3,13 +3,43 @@ package com.scorpion.unithalluwa.Assingment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.scorpion.unithalluwa.R;
 
 public class EditAssignment extends AppCompatActivity {
 
+    Button editAssBtn,deleteAssBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        editAssBtn = findViewById(R.id.editAssBtn);
+        deleteAssBtn = findViewById(R.id.deletebtn);
+
+        editAssBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //update code
+
+
+            }
+        });
+
+        deleteAssBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //delete code
+
+            }
+        });
+
     }
 }
+
+
